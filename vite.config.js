eb02f58b14e0dc29bfed64a3b02713150a9a7bfa
Danel Narvaiza <danelnarva12@gmail.com>
2026-05-17 15:03:21 +0200

    terminado

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/business_map/',
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
